574070cb15e9a45f5e36f2dc05d8b2a39c057d37
Danie <danielg768@gmail.com>
2020-02-10 16:37:02 -0500

    Updates

---
+++ precache-manifest.f662302325d4f03c7edc4ed72fe6c80c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c20ed36d5d913a1d1807b7a13d9be9a2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "befe863ba5fa6ac5c9f1",
+    "url": "/static/css/main.6a32922f.chunk.css"
+  },
+  {
+    "revision": "580684e474ae2b0c7a85",
+    "url": "/static/js/2.6256a66e.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/static/js/2.6256a66e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "befe863ba5fa6ac5c9f1",
+    "url": "/static/js/main.29a2df77.chunk.js"
+  },
+  {
+    "revision": "114476b61fc3d886f9ef",
+    "url": "/static/js/runtime-main.fc5ce8c6.js"
+  }
+]);
